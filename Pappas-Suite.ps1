9710e558824714c81994157337ae2a97d70f461c
John Pappas <163186936+pappasjfed@users.noreply.github.com>
2024-11-27 19:27:50 -0700

    Add OEL9 Vagrantfile

--- Pappas-Suite.ps1
+++
@@ -1,5 +0,0 @@
-# Driver Suite
-winget install DisplayLink.GraphicsDriver Poly.PlantronicsHub Logitech.Options Logitech.OptionsPlus
-
-# Apps Suite
-winget install Microsoft.PowerToys RamenSoftware.7+TaskbarTweaker Bitwarden.Bitwarden Bitwarden.CLI Ditto.Ditto
